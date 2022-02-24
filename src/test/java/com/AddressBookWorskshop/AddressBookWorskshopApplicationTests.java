cb07d6d5963a14528b34af2ab4147822fab44c0d
abhi9971 <abhishekrajawat101gmail.com>
2022-02-24 18:50:18 +0530

    Creating a Address Book project

---
+++ src/test/java/com/AddressBookWorskshop/AddressBookWorskshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AddressBookWorskshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookWorskshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
